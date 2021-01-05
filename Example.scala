de6131a5bcca10278777df4ed469c7b3f70a46e5
richardmcewan <richmcewan@hotmail.com>
2021-01-05 01:38:04 +0000

    Update Example.scala

--- Example.scala
+++ Example.scala
@@ -1,6 +1,34 @@
+//example
+//check name file and name class detail
+//check compile via intellij idea and terminal 
+// scan about for semi-colon customs
+
 Import java.io._
 
-class Person(val name
+//class name and constructor 
+class Person(val name: String, val age: Int){
 
-Object Example(Strings[] args){
+//fields
+var name : String = name;
+var age : Int = age;
+
+def futureAge(val year : Int){
+    //equivalent to this.age?????
+
+//concat like that?
+Println("You will be " + \
+        (year-2021+41) + \
+         " in = " + year)
+     }
+}
+
+Object Example{
+ def main(args: Array[String]){
+
+// create instance
+val mcewan = new Person("Rich", 41);
+
+//method
+mcewan.futureAge(2050);
+   }
 }
